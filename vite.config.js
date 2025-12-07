0728439c1f3bd00ab00529d443b093920a4ea4f9
LyubovNesterchuk <nesterchuklyubov@gmail.com>
2025-12-07 17:59:35 +0200

    fix

--- vite.config.js
+++ vite.config.js
@@ -6,19 +6,19 @@ import glob from "fast-glob";
 import { fileURLToPath } from "url";
 import { ViteImageOptimizer } from "vite-plugin-image-optimizer";
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 export default defineConfig({
+  base: "/sass-dashboard/", 
+
   plugins: [
     ViteImageOptimizer({
-      png: {
-        quality: 86,
-      },
-      jpeg: {
-        quality: 86,
-      },
-      jpg: {
-        quality: 86,
-      },
+      png: { quality: 86 },
+      jpeg: { quality: 86 },
+      jpg: { quality: 86 },
     }),
+
     {
       ...imagemin(["./src/img/**/*.{jpg,png,jpeg}"], {
         destination: "./src/img/webp/",
@@ -27,18 +27,18 @@ export default defineConfig({
       apply: "serve",
     },
   ],
+
   build: {
-    minify: false, // disable minification
+    minify: false,
     rollupOptions: {
       input: Object.fromEntries(
         glob
           .sync(["./*.html", "./pages/**/*.html"])
           .map((file) => [
-            path.relative(__dirname, file.slice(0, file.length - path.extname(file).length)),
+            path.relative(__dirname, file).replace(/\.html$/, ""),
             fileURLToPath(new URL(file, import.meta.url)),
           ])
       ),
-      // output unminified CSS file
       output: {
         assetFileNames: "assets/[name].[ext]",
       },
